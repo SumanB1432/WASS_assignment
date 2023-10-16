be192688f00ec6215966b7f3c37afe1e6055e8c0
Suman <suman80bera@gmail.com>
2023-10-16 19:52:22 +0530

    items_creation_api

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dotnev = require("dotenv").config();
-require("../db/config")
+require("../db/config");
+const item = require("../db/item");
 
 app.use(express.json());
 app.use(cors());
@@ -11,17 +12,25 @@ app.get("/",(req,res)=>{
     res.send("app is working")
 })
 
-app.post("POST/app/items",async(req,res)=>{
+app.post("/app/items",async(req,res)=>{
     try {
         let data = req.body;
         if(!data){
             res.status(400).send("Please provide Items details!")
         }
-        
+        let insertItem = await item.create(data);
+        if(insertItem){
+            console.log("hello")
+            res.status(201).send("Item Created Successfully")
+        }
+        else{
+            res.status(400).send("Something Wrong!")
+        }
     } catch (error) {
         if(error){
             res.status(500).send("Internal Server Error!")
         }
+
     }
 })
 
@@ -29,5 +38,6 @@ app.listen(process.env.PORT,(err)=>{
     if(!err){
         console.log(`SERVER RUN ON ${process.env.PORT}`)
     }
+
 
 })
