52ebce8c6c00653bafe2538b514b9fb97240a954
Suman <suman80bera@gmail.com>
2023-10-16 19:18:15 +0530

    db_configaration

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dotnev = require("dotenv").config();
+require("../db/config")
 
+app.use(express.json());
+app.use(cors());
 
+app.get("/",(req,res)=>{
+    res.send("app is working")
+})
+
+app.post("POST/app/items",async(req,res)=>{
+    try {
+        let data = req.body;
+        if(!data){
+            res.status(400).send("Please provide Items details!")
+        }
+        
+    } catch (error) {
+        if(error){
+            res.status(500).send("Internal Server Error!")
+        }
+    }
+})
 
 app.listen(process.env.PORT,(err)=>{
     if(!err){
